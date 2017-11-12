76247054a5d6c5da981875f8e0740ef5b799f32b
techhk <techhk@users.noreply.github.com>
2017-11-12 12:22:15 -0500

    Add SQL string function logic

--- DK_Update_org.sql
+++ DK_Update_org.sql
@@ -15,3 +15,22 @@ from DK_FCCorgsstandardized
 where neworgname is not null
 and (orgnamecrp is null or orgnamecrp='UBER')
 order by docid
+
+--For the records that doesn't get match using the python code, Run SQL string functions 
+--Some are in the code below.
+--Keep runing the search until you find satistfactory match.
+--Use STUFF, PATINDEX, CHARINDEX, REPLACE, SUBSTRING, LEFT and RIGHT to run string manipulation.
+
+select t1.id,t1.url,substring(content,CHARINDEX('Advertiser',content,1)+10,250)
+--charindex('Advertiser                                      Product                           Estimate Number Billing Address:                                                        ',content,1)
+from DK_documents t1
+join FCCorgsstandardized t2 on t1.id=t2.docID
+where t2.orgnamecrp is null
+and t1.content like '%Advertiser%Product%Estimate Number%'
+
+select t1.id,t1.url,substring(content,CHARINDEX('Advertiser',content,1)+10,250)
+--charindex('Advertiser                                      Product                           Estimate Number Billing Address:                                                        ',content,1)
+from DK_documents t1
+join FCCorgsstandardized t2 on t1.id=t2.docID
+where t2.orgnamecrp is null
+and t1.content like '%Advertiser%'
